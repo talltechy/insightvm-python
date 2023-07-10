1d32b8a27b4361010e834ee199332ab8a56e5a58
Matt <matt@mattwyen.me>
2023-07-10 18:18:37 -0400

    update

--- src/rapid7/api_r7_isvm_get_assets.py
+++ src/rapid7/api_r7_isvm_get_assets.py
@@ -12,100 +12,130 @@ logging.basicConfig(
     filename='api_r7_isvm_get_assets.log',
     filemode='a',
     format='%(asctime)s - %(levelname)s - %(message)s',
-    level=logging.INFO
+    level=logging.DEBUG # Change the logging level to DEBUG
 )
 
-def create_database():
+# Define the endpoint for retrieving assets from the InsightVM API
+ASSETS_ENDPOINT = '/api/3/assets'
+
+# Define the maximum number of assets to retrieve per page
+PAGE_SIZE = 50
+
+def get_all_assets():
     """
-    Create a new SQLite database and table to store the assets.
+    Retrieves all assets from the InsightVM API and returns them as a list of dictionaries.
     """
-    try:
-        conn = sqlite3.connect('assets.db')
-        cursor = conn.cursor()
-        cursor.execute('''CREATE TABLE IF NOT EXISTS assets
-                     (id INTEGER PRIMARY KEY,
-                     hostname TEXT,
-                     ip_address TEXT,
-                     os TEXT,
-                     last_scan_date TEXT)''')
-        conn.commit()
-        conn.close()
-    except sqlite3.Error as error:
-        logging.error("Failed to create database.")
-        logging.error(error)
-        raise
+    # Get the ISVM API credentials and base URL from environment variables
+    _, _, isvm_base_url = load_r7_isvm_api_credentials()
 
-def insert_assets(assets):
-    """
-    Insert the assets into the SQLite database.
-    """
-    try:
-        conn = sqlite3.connect('assets.db')
-        cursor = conn.cursor()
+    # Define the headers for the API request
+    headers = {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json',
+        'Authorization': get_isvm_basic_auth_header()
+    }
 
-        # Loop through each asset and insert it into the database
-        for asset in assets:
-            cursor.execute(
-                "INSERT INTO assets (id, hostname, ip_address, os, last_scan_date) VALUES (?, ?, ?, ?, ?)",
-                (
-                    asset['id'],
-                    asset['host_name'],
-                    asset['ip_address'],
-                    asset['os'],
-                    asset['last_scan_date']
-                )
-            )
+    # Define the parameters for the API request
+    params = {
+        'size': PAGE_SIZE,
+        'page': 0
+    }
 
-        conn.commit()
-        conn.close()
-    except sqlite3.Error as error:
-        logging.error("Failed to insert assets into the database.")
-        logging.error(error)
-        raise
+    # Initialize the list of assets
+    assets = []
 
-def get_assets():
-    """
-    Retrieve the assets from the Rapid7 InsightVM API.
-    """
-    try:
-        # Get the ISVM API credentials and base URL from environment variables
-        _, _, isvm_base_url = load_r7_isvm_api_credentials()
+    # Loop through all pages of assets
+    while True:
+        # Make the API request
+        response = requests.get(isvm_base_url + ASSETS_ENDPOINT, headers=headers, params=params)
 
-        # Construct the URL for the assets endpoint
-        assets_url = (
-            f"{isvm_base_url}/api/3/assets"
-            "?limit=50"
-        )
-
-        # Set up the request headers
-        headers = get_isvm_basic_auth_header()
-
-        # Make a GET request to the assets endpoint to retrieve all assets
-        response = requests.get(
-            assets_url,
-            headers=headers,
-            timeout=10, # Add a timeout argument to avoid hanging indefinitely
-            verify=False # Ignore SSL errors for testing purposes
-        )
-
-        # Check if the request was successful
+        # Check if the API request was successful
         if response.status_code != 200:
-            logging.error("Failed to retrieve assets.")
-            raise ValueError("Failed to retrieve assets.")
+            logging.error('Failed to retrieve assets from the InsightVM API: %s', response.text)
+            break
 
-        # Parse the JSON response
-        assets = json.loads(response.content)
+        # Parse the API response
+        data = json.loads(response.text)
 
-        # Insert the assets into the database
-        insert_assets(assets)
-        logging.info("Assets retrieved and inserted into the database.")
-    except requests.RequestException as error:
-        logging.error("Failed to retrieve assets from the API.")
-        logging.error(error)
-        raise
+        # Add the assets from the current page to the list
+        assets.extend(data['assets'])
 
-# Call the create_database function to create a new database and table
-create_database()
+        # Check if there are more pages of assets
+        if data['pagination']['next_page'] is None:
+            break
 
-# Call the get_assets function to retrieve the assets and insert them into the database
-get_assets()
+        # Update the parameters for the next page of assets
+        params['page'] = data['pagination']['next_page']
+
+    return assets
+
+def create_assets_table(conn):
+    """
+    Creates the assets table in the SQLite database.
+    """
+    cursor = conn.cursor()
+    cursor.execute('''
+        CREATE TABLE IF NOT EXISTS assets (
+            id TEXT PRIMARY KEY,
+            hostname TEXT,
+            ip_address TEXT,
+            mac_address TEXT,
+            fqdn TEXT,
+            netbios_name TEXT,
+            operating_system TEXT,
+            last_assessed TEXT
+        )
+    ''')
+    conn.commit()
+
+def insert_assets(conn, assets):
+    """
+    Inserts the specified assets into the assets table in the SQLite database.
+    """
+    cursor = conn.cursor()
+    for asset in assets:
+        cursor.execute('''
+            INSERT OR REPLACE INTO assets (
+                id,
+                hostname,
+                ip_address,
+                mac_address,
+                fqdn,
+                netbios_name,
+                operating_system,
+                last_assessed
+            ) VALUES (?, ?, ?, ?, ?, ?, ?, ?)
+        ''', (
+            asset['id'],
+            asset['host_name'],
+            asset['ip_address'],
+            asset['mac_address'],
+            asset['fqdn'],
+            asset['netbios_name'],
+            asset['operating_system'],
+            asset['last_assessed']
+        ))
+    conn.commit()
+
+def main():
+    """
+    Retrieves all assets from the InsightVM API and stores them in a SQLite database.
+    """
+    # Connect to the SQLite database
+    conn = sqlite3.connect('assets.db')
+
+    # Create the assets table if it doesn't exist
+    create_assets_table(conn)
+
+    # Retrieve all assets from the InsightVM API
+    assets = get_all_assets()
+
+    # Insert the assets into the SQLite database
+    insert_assets(conn, assets)
+
+    # Close the database connection
+    conn.close()
+
+if __name__ == '__main__':
+    main()
+
