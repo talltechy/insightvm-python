dc9612ce859313e28905790bab56b7664c2d5cae
Matt Wyen <matt@mattwyen.me>
2025-10-10 12:15:39 -0400

    clean up old files.

--- trial_and_error/get_insight_agents.py
+++
@@ -1,7 +0,0 @@
-from trial_and_error.insight_agents import get_insight_agents
-
-# Retrieve the first page of agents with a page size of 50
-agents = get_insight_agents(page=1, page_size=50)
-
-# Print the list of agents
-print(agents)
